a0e98b6e55a79134ea985aa39a73870f3f0232e5
MassimoI <massimoi@nkoni.org>
2021-06-03 10:41:00 +0200

    correzione carpooling hub

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -16,7 +16,7 @@
 
 use Cake\Core\Configure;
 
-$cakeDescription = __("Car Pooling HUB - Servizi Online CSI");
+$cakeDescription = __("Carpooling hub - Servizi Online CSI");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
@@ -29,8 +29,7 @@ $cakeDescription = __("Car Pooling HUB - Servizi Online CSI");
     <link href="<?php echo __("favicon_url");?>" rel="shortcut icon" type="image/vnd.microsoft.icon">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>
-        <?= $cakeDescription ?>:
-        <?= $this->fetch('title') ?>
+        <?= $cakeDescription ?>
     </title>
     <?= $this->Html->meta('icon') ?>
 
