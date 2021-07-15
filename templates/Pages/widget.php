23c49c672a37ffbca661331760dd01b30d200d96
MassimoI <massimoi@nkoni.org>
2021-07-15 15:47:00 +0200

    caricamento pagina widget

---
+++ templates/Pages/widget.php
@@ -0,0 +1,70 @@
+<h1>Inserisci il widget CarPooling Hub nel tuo sito</h1>
+
+<p>Puoi inserire il widget Car Pooling Hub nel tuo sito in modo da favorire la ricerca di passaggi in car pooling, senza doverti legare ad uno specifico fornitore.</p>
+<p>
+    Nell’ambito del progetto Opticities, CSI Piemonte ha realizzato Carpooling hub, un aggregatore di siti per il carpooling destinato a:
+<ul>
+    <li>cittadini disponibili a condividere un viaggio in automobile,</li>
+    <li>pubbliche amministrazioni che, integrando Carpooling hub nel proprio sito istituzionale possono promuovere il carpooling, nel rispetto della concorrenza tra diversi siti di carpooling,</li>
+    <li>gestori di siti di carpooling, che aderendo all'aggregatore, possono dare maggiore visibilità al proprio sito.</li>
+</ul>
+E' possibile, inoltre, scaricare gratuitamente un widget e alcuni pulsanti web da integrare in una qualsiasi pagina web, in modo da diffondere il più possibile l’uso del car pooling come modalità di trasporto sostenibile.
+
+Questo documento fornisce le indicazioni per tale integrazione.
+
+Per informazioni o segnalazioni è possibile scrivere a <a href="mailto:assistenza.trasporti@csi.it">assistenza.trasporti@csi.it</a>
+</ul>
+</p>
+
+
+
+<h2>Incorporare il Widget</h2>
+Modifica la pagina del tuo sito web nel punto in cui desideri inserire il widget.
+
+Il codice da inserire nella pagina è il seguente:<br>
+
+<pre>
+&lt;iframe width="400" height="600" src="https://www.carpoolinghub.it/ita/journeys/search/journeys/search?layout=embed"&gt;&lt;/iframe&gt;
+</pre>
+
+<iframe width="400" height="600" src="https://www.carpoolinghub.it/ita/journeys/search/journeys/search?layout=embed"></iframe>
+
+
+<hr>
+
+
+<h2>Pulsante con Link</h2>
+Se invece preferisci inserire un pulsante che mandi i tuoi visitatori a Car Pooling Hub, questo è il codice
+
+<h3>Pulsante Piccolo</h3>
+<pre>
+&lt;a&nbsp;href=&quot;https://www.carpoolinghub.it/journeys/search&quot;&gt;
+&nbsp;&nbsp;&lt;img&nbsp;alt=&quot;pulsante&nbsp;carpooling&nbsp;hub&nbsp;small&quot;&nbsp;src=&quot;https://www.carpoolinghub.it/img/carpool_small.png&quot;&nbsp; /&gt;
+&lt;/a&gt;
+</pre>
+<a href="https://www.carpoolinghub.it/journeys/search">
+      <img alt="pulsante carpooling hub small" src="https://www.carpoolinghub.it/img/carpool_small.png"  />
+</a>
+
+<h3>Pulsante Grande</h3>
+<pre>
+&lt;a&nbsp;href=&quot;https://www.carpoolinghub.it/journeys/search&quot;&gt;
+&nbsp;&nbsp;&lt;img&nbsp;alt=&quot;pulsante&nbsp;carpooling&nbsp;hub&nbsp;medium&quot;&nbsp;src=&quot;https://www.carpoolinghub.it/img/carpool_big.png&quot;&nbsp; /&gt;
+&lt;/a&
+gt;
+</pre>
+<a href="https://www.carpoolinghub.it/journeys/search">
+      <img alt="pulsante carpooling hub medium" src="https://www.carpoolinghub.it/img/carpool_big.png"  />
+</a>
+
+<h3>Logo</h3>
+<pre>
+&lt;a&nbsp;href=&quot;https://www.carpoolinghub.it/journeys/search&quot;&gt;
+&nbsp;&nbsp;&lt;img&nbsp;alt=&quot;pulsante&nbsp;carpooling&nbsp;hub&nbsp;logo&quot;&nbsp;src=&quot;https://www.carpoolinghub.it/img/carpooling_logo.png&quot;&nbsp; /&gt;
+&lt;/a&gt;
+</pre>
+
+<a href="https://www.carpoolinghub.it/journeys/search">
+      <img alt="pulsante carpooling hub logo" src="https://www.carpoolinghub.it/img/carpooling_logo.png"  />
+</a>
+
